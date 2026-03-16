bf96d0f0c263cb961565845085b505e3052c9e13
unknown <arsh2005veer@gmail.com>
2026-03-16 11:00:46 +0530

    reading angles for interpolation

--- angles_inter.py
+++ angles_inter.py
@@ -1,23 +1,40 @@
 '''Parsing the xml file for timestamps and line exposure time,
 and also finding the namespace for isda(line exposure)
 '''
-xml=r"/home/megha/arshveer/ch2_iir_nci_20250529T1233369467_d_img_d18/data/calibrated/20250529/ch2_iir_nci_20250529T1233369467_d_img_d18.xml"
+xml=r"D:\ch2_iir_nci_20210720T2333026105_d_img_d32\data\calibrated\20210720\ch2_iir_nci_20210720T2333026105_d_img_d32.xml"
 
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET, os,csv
 from datetime import datetime
-
+os.system('cls')
 tree=ET.parse(xml)
 root=tree.getroot()
-namespace=root.tag
+#namespace=root.tag
 #print(namespace)
+ns={'isda':'http://pds.nasa.gov/pds4/pds/v1'}   #namespace uri stored in dict coz every element has it before its name
 
 for elem in root.iter():
-   if "start_date_time" in elem.tag:
-        print(elem.tag)
+   tag=(elem.tag).split("}")[-1]
+   if "line_exposure_duration"==tag:
+        line=float(elem.text)
+        print(line)
 
-start_time_st=root.find('.//start_date_time').text
+
+start_time_st=root.find('.//isda:start_date_time',ns).text
+print(start_time_st)
 time=datetime.strptime(
     start_time_st,
-    "%Y%m%dT%H:%M:%S.%fZ"
+    "%Y-%m-%dT%H:%M:%S.%fZ"
 ).timestamp()
 print(time)
+
+#updated csv for reading the required angles
+c=r"D:\ch2_iir_nci_20210720T2333026105_d_img_d32\oat1.csv"
+with open(c,"r") as f:
+    header=csv.reader(c)
+    a=header.index("Solar_Azimuth")
+    z=header.index("Solar_Zenith_Angle")
+    sz=header.index("Yaw_Nadir_Angle")
+    sa=header
+    for line in f:
+        az=
+
